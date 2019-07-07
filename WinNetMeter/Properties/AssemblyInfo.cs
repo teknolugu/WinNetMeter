b95a53e6178e9d6b6392190b3dedf4cad3bdaed4
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-07 19:31:39 +0700

    Added assembly description and company name

--- WinNetMeter/Properties/AssemblyInfo.cs
+++ WinNetMeter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinNetMeter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A realtime network speed monitor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WinTenDev Inc.")]
 [assembly: AssemblyProduct("WinNetMeter")]
 [assembly: AssemblyCopyright("Copyright ©  2019 WinTenDev Inc.")]
 [assembly: AssemblyTrademark("")]

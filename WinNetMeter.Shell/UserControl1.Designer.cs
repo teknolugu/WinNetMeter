9bcd2137c9d9ae87eaad9bac4f01d892ed64ea85
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-09 18:30:44 +0700

    Fix some issue on toolbar UI

--- WinNetMeter.Shell/UserControl1.Designer.cs
+++ WinNetMeter.Shell/UserControl1.Designer.cs
@@ -34,15 +34,22 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.LblDownload = new WinNetMeter.Shell.MyLabel();
             this.LblUpload = new WinNetMeter.Shell.MyLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.configurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitoringToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.offToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictDownload)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictUpload)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictDownload
             // 
             this.pictDownload.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.pictDownload.Image = global::WinNetMeter.Shell.Properties.Resources.down_white_16px;
-            this.pictDownload.Location = new System.Drawing.Point(3, 15);
+            this.pictDownload.Location = new System.Drawing.Point(3, 16);
             this.pictDownload.Name = "pictDownload";
             this.pictDownload.Size = new System.Drawing.Size(12, 15);
             this.pictDownload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -53,7 +60,7 @@
             // 
             this.pictUpload.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.pictUpload.Image = global::WinNetMeter.Shell.Properties.Resources.up_white_16px;
-            this.pictUpload.Location = new System.Drawing.Point(3, 2);
+            this.pictUpload.Location = new System.Drawing.Point(3, 4);
             this.pictUpload.Name = "pictUpload";
             this.pictUpload.Size = new System.Drawing.Size(12, 15);
             this.pictUpload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -69,7 +76,7 @@
             this.LblDownload.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.LblDownload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblDownload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LblDownload.Location = new System.Drawing.Point(30, 16);
+            this.LblDownload.Location = new System.Drawing.Point(30, 15);
             this.LblDownload.Name = "LblDownload";
             this.LblDownload.Size = new System.Drawing.Size(68, 12);
             this.LblDownload.TabIndex = 6;
@@ -82,31 +89,77 @@
             this.LblUpload.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.LblUpload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblUpload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LblUpload.Location = new System.Drawing.Point(30, 1);
+            this.LblUpload.Location = new System.Drawing.Point(30, 2);
             this.LblUpload.Name = "LblUpload";
             this.LblUpload.Size = new System.Drawing.Size(68, 12);
             this.LblUpload.TabIndex = 5;
             this.LblUpload.Text = "......";
             this.LblUpload.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.LblUpload.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.LblUpload.Click += new System.EventHandler(this.LblUpload_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configurationToolStripMenuItem,
+            this.monitoringToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 70);
+            // 
+            // configurationToolStripMenuItem
+            // 
+            this.configurationToolStripMenuItem.Name = "configurationToolStripMenuItem";
+            this.configurationToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.configurationToolStripMenuItem.Text = "Configuration";
+            this.configurationToolStripMenuItem.Click += new System.EventHandler(this.ConfigurationToolStripMenuItem_Click);
+            // 
+            // monitoringToolStripMenuItem
+            // 
+            this.monitoringToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onToolStripMenuItem,
+            this.offToolStripMenuItem});
+            this.monitoringToolStripMenuItem.Name = "monitoringToolStripMenuItem";
+            this.monitoringToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.monitoringToolStripMenuItem.Text = "Monitoring";
+            // 
+            // onToolStripMenuItem
+            // 
+            this.onToolStripMenuItem.Name = "onToolStripMenuItem";
+            this.onToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.onToolStripMenuItem.Text = "On";
+            this.onToolStripMenuItem.Click += new System.EventHandler(this.OnToolStripMenuItem_Click);
+            // 
+            // offToolStripMenuItem
+            // 
+            this.offToolStripMenuItem.Name = "offToolStripMenuItem";
+            this.offToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.offToolStripMenuItem.Text = "Off";
+            this.offToolStripMenuItem.Click += new System.EventHandler(this.OffToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
             // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Black;
+            this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.LblDownload);
             this.Controls.Add(this.LblUpload);
             this.Controls.Add(this.pictUpload);
             this.Controls.Add(this.pictDownload);
             this.Name = "UserControl1";
-            this.Size = new System.Drawing.Size(101, 33);
+            this.Size = new System.Drawing.Size(101, 38);
             this.Load += new System.EventHandler(this.UserControl1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictDownload)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictUpload)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -117,5 +170,11 @@
         private System.Windows.Forms.Timer timer1;
         private MyLabel LblUpload;
         private MyLabel LblDownload;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configurationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monitoringToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem onToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem offToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }

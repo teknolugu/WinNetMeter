2c28bb7b29979110390c5dfdf98ce4a6349f799e
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-09 18:30:22 +0700

    General config and style config load on different method

--- WinNetMeter.Shell/UserControl1.cs
+++ WinNetMeter.Shell/UserControl1.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Threading;
 using WinNetMeter.Shell.Model;
 using System.Drawing.Text;
+using System.Diagnostics;
 
 namespace WinNetMeter.Shell
 {
@@ -19,6 +20,7 @@ namespace WinNetMeter.Shell
         private RegistryManager registryManager = new RegistryManager();
         private StyleConfiguration styleConfiguration;
         private DbManager dataManager = new DbManager();
+        private Configuration generalConfiguration;
 
         public UserControl1(CSDeskBand.CSDeskBandWin w)
         {
@@ -39,15 +41,45 @@ namespace WinNetMeter.Shell
         {
             this.Controls.Clear();
             InitializeComponent();
-            UserControl1_Load(new EventArgs(), new EventArgs());
+            Load_Config();
             ConfigureStyle();
         }
 
         private void UserControl1_Load(object sender, EventArgs e)
         {
             registryManager.SaveHwnd(this.Handle.ToString());
-            
-            //get taskbar color
+
+            ConfigureStyle();
+            Load_Config();
+        }
+
+        private void Load_Config()
+        {
+            generalConfiguration = registryManager.GetGeneralConfiguration();
+
+            if (generalConfiguration.Monitoring == true)
+            {
+                Format = generalConfiguration.Format;
+
+                adapterController = new AdapterController(generalConfiguration.MonitoredAdapter);
+                monitor = new NetworkMonitor(adapterController);
+                monitor.Start();
+                timer1.Start();
+            }
+        }
+
+        private void ConfigureStyle()
+        {
+
+            this.BackColor = ColorTranslator.FromHtml("#000");
+
+            styleConfiguration = registryManager.GetStyleConfiguration();
+
+            LblDownload.ForeColor = ColorTranslator.FromHtml(styleConfiguration.TextColor);
+            LblUpload.ForeColor = ColorTranslator.FromHtml(styleConfiguration.TextColor);
+
+            LblDownload.Font = new Font(styleConfiguration.FontFamily, LblDownload.Font.Size);
+            LblUpload.Font = new Font(styleConfiguration.FontFamily, LblUpload.Font.Size);
 
             var taskBar = new TaskBarHelper();
             Color taskBarColor = taskBar.GetColourAt(taskBar.GetTaskbarPosition().Location);
@@ -59,14 +91,14 @@ namespace WinNetMeter.Shell
                 pictDownload.Image = Properties.Resources.down_black_16px;
                 pictUpload.Image = Properties.Resources.up_black_16px;
 
-                pictDownload.Location = new Point(10, 14);
+                pictDownload.Location = new Point(10, 15);
             }
             else if (styleConfiguration.Icon == IconStyle.Arrow && IsDark)
             {
                 pictDownload.Image = Properties.Resources.down_white_16px;
                 pictUpload.Image = Properties.Resources.up_white_16px;
 
-                pictDownload.Location = new Point(10, 14);
+                pictDownload.Location = new Point(10, 15);
             }
             else if (styleConfiguration.Icon == IconStyle.TriangleArrow && IsDark == false)
             {
@@ -91,33 +123,8 @@ namespace WinNetMeter.Shell
                 pictUpload.Image = Properties.Resources.outline_arrow_up_white_16px;
 
             }
-
-            var config = registryManager.GetGeneralConfiguration();
-
-            if (config.Monitoring == true)
-            {
-                Format = config.Format;
-
-                adapterController = new AdapterController(config.MonitoredAdapter);
-                monitor = new NetworkMonitor(adapterController);
-                monitor.Start();
-                timer1.Start();
-            }
         }
-
-        private void ConfigureStyle()
-        {
-            this.BackColor = ColorTranslator.FromHtml("#000");
-
-            styleConfiguration = registryManager.GetStyleConfiguration();
-
-            LblDownload.ForeColor = ColorTranslator.FromHtml(styleConfiguration.TextColor);
-            LblUpload.ForeColor = ColorTranslator.FromHtml(styleConfiguration.TextColor);
-
-            LblDownload.Font = new Font(styleConfiguration.FontFamily, LblDownload.Font.Size);
-            LblUpload.Font = new Font(styleConfiguration.FontFamily, LblUpload.Font.Size);
-        }
-
+        
         private void Timer1_Tick(object sender, EventArgs e)
         {
             switch (Format)
@@ -143,12 +150,32 @@ namespace WinNetMeter.Shell
             }
         }
 
-        private void LblUpload_Click(object sender, EventArgs e)
+        private void ConfigurationToolStripMenuItem_Click(object sender, EventArgs e)
         {
+
+                Process.Start(registryManager.GetExecutableLocation());
+        }
+
+        private void OnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            registryManager.Save("Monitoring", "True", ConfigurationType.GeneralConfiguration);
             RestartDesk();
         }
 
+        private void OffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            registryManager.Save("Monitoring", "False", ConfigurationType.GeneralConfiguration);
+            timer1.Stop();
+            RestartDesk();
+        }
 
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = registryManager.GetExecutableLocation();
+            startInfo.Arguments = "-about";
+            Process.Start(startInfo);
+        }
     }
     public class MyLabel : Label
     {

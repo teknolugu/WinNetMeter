7631663aaa9919ffaaa7533f13fbe277b9cb2353
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-11 15:08:57 +0700

    Open link with default browser

--- WinNetMeter/Main.cs
+++ WinNetMeter/Main.cs
@@ -331,5 +331,44 @@ namespace WinNetMeter
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        #region Link
+
+        private void LinkArfan_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.facebook.com/MArfan890");
+        }
+
+        private void LinkAzhe_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.facebook.com/azhe403");
+        }
+
+        private void LinkJeremy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.facebook.com/SiiJere");
+        }
+
+        private void LinkJovan_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.facebook.com/jovanzers");
+        }
+
+        private void LinkWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.winten.tk");
+        }
+
+        private void LinkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/WinTenDev");
+        }
+
+        private void LinkDonation_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FWM2D25VHMERG&source=url");
+        }
+
+        #endregion
     }
 }

4401ae34506dd5f643d2440ad34a68f213d21a7d
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-15 10:13:33 +0700

    Add WM_MONITOROFF commands

--- WinNetMeter.Shell/Helper/NativeMethods.cs
+++ WinNetMeter.Shell/Helper/NativeMethods.cs
@@ -11,6 +11,7 @@ namespace WinNetMeter.Shell.Helper
     {
         public const int HWND_BROADCAST = 0xffff;
         public static readonly int WM_RESTART = RegisterWindowMessage("WM_RESTART");
+        public static readonly int WM_OFFMONITOR = RegisterWindowMessage("WM_OFFMONITOR");
         [DllImport("user32")]
         public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
         [DllImport("user32")]

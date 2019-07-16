db98071a5388c0301c17eae3b4ed7250875717ff
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-16 22:10:20 +0700

    Add update model

---
+++ WinNetMeter/Model/Update.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinNetMeter.Model
+{
+    class Update
+    {
+        private string dashboardVersion;
+        private string updaterVersion;
+        private string shellVersion;
+        private string description;
+        private List<string> changelog;
+
+        public List<string> Changelog { get => changelog; set => changelog = value; }
+        public string ShellVersion { get => shellVersion; set => shellVersion = value; }
+        public string UpdaterVersion { get => updaterVersion; set => updaterVersion = value; }
+        public string DashboardVersion { get => dashboardVersion; set => dashboardVersion = value; }
+    } 
+}

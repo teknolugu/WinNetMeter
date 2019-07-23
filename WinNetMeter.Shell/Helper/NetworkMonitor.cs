6f979dacf3b252596979c5c73ca7a6957a292be2
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-23 13:24:40 +0700

    Improve Network Monitor core integration by Implementing IDisposable on the class

--- WinNetMeter.Shell/Helper/NetworkMonitor.cs
+++ WinNetMeter.Shell/Helper/NetworkMonitor.cs
@@ -1,13 +1,18 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Timers;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
 using WinNetMeter.Shell.Controller;
 
 namespace WinNetMeter.Shell.Helper
 {
-    internal class NetworkMonitor
+    internal class NetworkMonitor : IDisposable
     {
         private AdapterController adapter;
         private Timer monitor;
+        bool disposed = false;
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
 
         public NetworkMonitor(AdapterController adapterC)
         {
@@ -16,6 +21,25 @@ namespace WinNetMeter.Shell.Helper
             monitor = new Timer();
             monitor.Interval = 1000;
             monitor.Elapsed += Monitor_Elapsed;
+        }
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+            disposed = true;
         }
 
         private void Monitor_Elapsed(object sender, ElapsedEventArgs e)
@@ -45,6 +69,10 @@ namespace WinNetMeter.Shell.Helper
                 adapter.Initialize();
                 monitor.Enabled = true;
             }
+        }
+        public void Stop()
+        {
+            monitor.Enabled = false;
         }
     }
 }

a866702fa9b980f4da88aa840a6986223bfe51d9
Muchammad Arfan <muchammadarfan76@gmail.com>
2019-07-25 18:43:16 +0700

    Add show and hide shell option

--- WinNetMeter/UserControls/IntegrationPage.Designer.cs
+++ WinNetMeter/UserControls/IntegrationPage.Designer.cs
@@ -30,14 +30,20 @@
         {
             this.lineShape4 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.label12 = new System.Windows.Forms.Label();
             this.lineShape7 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label12 = new System.Windows.Forms.Label();
             this.label21 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.BtnUninstall = new System.Windows.Forms.Button();
             this.label13 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.BtnRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.BtnShow = new System.Windows.Forms.Button();
+            this.BtnHide = new System.Windows.Forms.Button();
+            this.LinkRefresh = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // lineShape4
@@ -61,6 +67,15 @@
             this.shapeContainer1.TabIndex = 0;
             this.shapeContainer1.TabStop = false;
             // 
+            // lineShape7
+            // 
+            this.lineShape7.BorderColor = System.Drawing.SystemColors.Control;
+            this.lineShape7.Name = "lineShape7";
+            this.lineShape7.X1 = 21;
+            this.lineShape7.X2 = 559;
+            this.lineShape7.Y1 = 60;
+            this.lineShape7.Y2 = 60;
+            // 
             // label12
             // 
             this.label12.AutoSize = true;
@@ -71,20 +86,11 @@
             this.label12.TabIndex = 45;
             this.label12.Text = "Integration";
             // 
-            // lineShape7
-            // 
-            this.lineShape7.BorderColor = System.Drawing.SystemColors.Control;
-            this.lineShape7.Name = "lineShape7";
-            this.lineShape7.X1 = 21;
-            this.lineShape7.X2 = 559;
-            this.lineShape7.Y1 = 60;
-            this.lineShape7.Y2 = 60;
-            // 
             // label21
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label21.Location = new System.Drawing.Point(31, 249);
+            this.label21.Location = new System.Drawing.Point(29, 219);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(135, 25);
             this.label21.TabIndex = 51;
@@ -94,7 +100,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label17.Location = new System.Drawing.Point(31, 71);
+            this.label17.Location = new System.Drawing.Point(29, 68);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(163, 25);
             this.label17.TabIndex = 50;
@@ -102,19 +108,25 @@
             // 
             // BtnUninstall
             // 
-            this.BtnUninstall.Location = new System.Drawing.Point(354, 343);
+            this.BtnUninstall.BackColor = System.Drawing.Color.Transparent;
+            this.BtnUninstall.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnUninstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnUninstall.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUninstall.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnUninstall.Location = new System.Drawing.Point(34, 295);
             this.BtnUninstall.Name = "BtnUninstall";
-            this.BtnUninstall.Size = new System.Drawing.Size(135, 40);
+            this.BtnUninstall.Size = new System.Drawing.Size(130, 34);
             this.BtnUninstall.TabIndex = 49;
             this.BtnUninstall.Text = "Uninstall Shell";
-            this.BtnUninstall.UseVisualStyleBackColor = true;
+            this.BtnUninstall.UseVisualStyleBackColor = false;
             this.BtnUninstall.Click += new System.EventHandler(this.BtnUninstall_Click);
             // 
             // label13
             // 
-            this.label13.Location = new System.Drawing.Point(33, 274);
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(31, 251);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(408, 35);
+            this.label13.Size = new System.Drawing.Size(412, 35);
             this.label13.TabIndex = 48;
             this.label13.Text = "If you want update or uninstall WinNetMeter, you can Uninstall Shell before, \r\nth" +
     "en replace with new Version.";
@@ -122,7 +134,7 @@
             // label18
             // 
             this.label18.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label18.Location = new System.Drawing.Point(31, 101);
+            this.label18.Location = new System.Drawing.Point(33, 101);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(410, 62);
             this.label18.TabIndex = 47;
@@ -132,19 +144,101 @@
             // 
             // BtnRegister
             // 
-            this.BtnRegister.Location = new System.Drawing.Point(354, 197);
+            this.BtnRegister.BackColor = System.Drawing.Color.Transparent;
+            this.BtnRegister.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRegister.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegister.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnRegister.Location = new System.Drawing.Point(36, 165);
             this.BtnRegister.Name = "BtnRegister";
-            this.BtnRegister.Size = new System.Drawing.Size(135, 40);
+            this.BtnRegister.Size = new System.Drawing.Size(128, 35);
             this.BtnRegister.TabIndex = 46;
             this.BtnRegister.Text = "Re-Register Shell";
-            this.BtnRegister.UseVisualStyleBackColor = true;
+            this.BtnRegister.UseVisualStyleBackColor = false;
             this.BtnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
             // 
-            // Integration
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(29, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Shell Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Status :";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(74, 380);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(63, 15);
+            this.lblStatus.TabIndex = 54;
+            this.lblStatus.Text = "Not Active";
+            // 
+            // BtnShow
+            // 
+            this.BtnShow.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BtnShow.FlatAppearance.BorderSize = 0;
+            this.BtnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnShow.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnShow.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnShow.Location = new System.Drawing.Point(34, 408);
+            this.BtnShow.Name = "BtnShow";
+            this.BtnShow.Size = new System.Drawing.Size(130, 35);
+            this.BtnShow.TabIndex = 55;
+            this.BtnShow.Text = "Show Shell";
+            this.BtnShow.UseVisualStyleBackColor = false;
+            this.BtnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // BtnHide
+            // 
+            this.BtnHide.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.BtnHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHide.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.BtnHide.Location = new System.Drawing.Point(175, 407);
+            this.BtnHide.Name = "BtnHide";
+            this.BtnHide.Size = new System.Drawing.Size(129, 36);
+            this.BtnHide.TabIndex = 56;
+            this.BtnHide.Text = "Hide Shell";
+            this.BtnHide.UseVisualStyleBackColor = true;
+            this.BtnHide.Click += new System.EventHandler(this.BtnHide_Click);
+            // 
+            // LinkRefresh
+            // 
+            this.LinkRefresh.AutoSize = true;
+            this.LinkRefresh.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinkRefresh.Location = new System.Drawing.Point(141, 360);
+            this.LinkRefresh.Name = "LinkRefresh";
+            this.LinkRefresh.Size = new System.Drawing.Size(46, 13);
+            this.LinkRefresh.TabIndex = 57;
+            this.LinkRefresh.TabStop = true;
+            this.LinkRefresh.Text = "Refresh";
+            this.LinkRefresh.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkRefresh_LinkClicked);
+            // 
+            // IntegrationPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.LinkRefresh);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.BtnHide);
+            this.Controls.Add(this.BtnShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label21);
             this.Controls.Add(this.label17);
             this.Controls.Add(this.BtnUninstall);
@@ -153,8 +247,9 @@
             this.Controls.Add(this.BtnRegister);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.shapeContainer1);
-            this.Name = "Integration";
+            this.Name = "IntegrationPage";
             this.Size = new System.Drawing.Size(568, 485);
+            this.Load += new System.EventHandler(this.IntegrationPage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,5 +267,11 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label18;
         private System.Windows.Forms.Button BtnRegister;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button BtnShow;
+        private System.Windows.Forms.Button BtnHide;
+        private System.Windows.Forms.LinkLabel LinkRefresh;
     }
 }
--- WinNetMeter/UserControls/IntegrationPage.cs
+++ WinNetMeter/UserControls/IntegrationPage.cs
@@ -6,6 +6,7 @@ namespace WinNetMeter.UserControls
 {
     public partial class IntegrationPage : UserControl
     {
+        private ShellController shellControlller = new ShellController();
         public IntegrationPage()
         {
             InitializeComponent();
@@ -41,6 +42,48 @@ namespace WinNetMeter.UserControls
                 MessageBox.Show($"Something happened. {ex.Message}", "WinNetMeter Integration",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void IntegrationPage_Load(object sender, EventArgs e)
+        {
+            CheckIsShellActive();
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            // Call the shell
+            shellControlller.callDeskband();
+
+            // Refresh shell status
+            CheckIsShellActive();
+        }
+
+        private void CheckIsShellActive()
+        {
+            if (shellControlller.IsShellShown() == 0)
+            {
+                lblStatus.Text = "Active";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblStatus.Text = "Not Active";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void BtnHide_Click(object sender, EventArgs e)
+        {
+            // Hide the shell
+            shellControlller.hideDeskband();
+
+            // Refresh shell status
+            CheckIsShellActive();
+        }
+
+        private void LinkRefresh_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            CheckIsShellActive();
         }
     }
 }

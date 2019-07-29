7431f893244aee68eeebee322f3219fb7cc9b0b3
Muhammad Arfan <muchammadarfan76@gmail.com>
2019-07-29 16:47:10 +0700

    Remove panel from shell UI

--- WinNetMeter.Shell/UserControl1.Designer.cs
+++ WinNetMeter.Shell/UserControl1.Designer.cs
@@ -29,11 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictDownload = new System.Windows.Forms.PictureBox();
-            this.pictUpload = new System.Windows.Forms.PictureBox();
             this.timerAuto = new System.Windows.Forms.Timer(this.components);
-            this.LblDownload = new WinNetMeter.Shell.MyLabel();
-            this.LblUpload = new WinNetMeter.Shell.MyLabel();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.configurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.monitoringToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,64 +38,18 @@
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.timerKB = new System.Windows.Forms.Timer(this.components);
             this.timerMB = new System.Windows.Forms.Timer(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictDownload = new System.Windows.Forms.PictureBox();
+            this.pictUpload = new System.Windows.Forms.PictureBox();
+            this.LblDownload = new WinNetMeter.Shell.MyLabel();
+            this.LblUpload = new WinNetMeter.Shell.MyLabel();
+            this.contextMenuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictDownload)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictUpload)).BeginInit();
-            this.contextMenuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictDownload
-            // 
-            this.pictDownload.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictDownload.Image = global::WinNetMeter.Shell.Properties.Resources.down_white_16px;
-            this.pictDownload.Location = new System.Drawing.Point(7, 18);
-            this.pictDownload.Name = "pictDownload";
-            this.pictDownload.Size = new System.Drawing.Size(12, 15);
-            this.pictDownload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictDownload.TabIndex = 2;
-            this.pictDownload.TabStop = false;
-            // 
-            // pictUpload
-            // 
-            this.pictUpload.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictUpload.Image = global::WinNetMeter.Shell.Properties.Resources.up_white_16px;
-            this.pictUpload.Location = new System.Drawing.Point(7, 4);
-            this.pictUpload.Name = "pictUpload";
-            this.pictUpload.Size = new System.Drawing.Size(12, 15);
-            this.pictUpload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictUpload.TabIndex = 3;
-            this.pictUpload.TabStop = false;
             // 
             // timerAuto
             // 
             this.timerAuto.Tick += new System.EventHandler(this.Timer1_Tick);
-            // 
-            // LblDownload
-            // 
-            this.LblDownload.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.LblDownload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblDownload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LblDownload.Location = new System.Drawing.Point(1, 18);
-            this.LblDownload.Name = "LblDownload";
-            this.LblDownload.Size = new System.Drawing.Size(77, 12);
-            this.LblDownload.TabIndex = 6;
-            this.LblDownload.Text = "......";
-            this.LblDownload.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            this.LblDownload.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            // 
-            // LblUpload
-            // 
-            this.LblUpload.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.LblUpload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblUpload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.LblUpload.Location = new System.Drawing.Point(1, 4);
-            this.LblUpload.Name = "LblUpload";
-            this.LblUpload.Size = new System.Drawing.Size(77, 12);
-            this.LblUpload.TabIndex = 5;
-            this.LblUpload.Text = "......";
-            this.LblUpload.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            this.LblUpload.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             // 
             // contextMenuStrip1
             // 
@@ -155,15 +105,55 @@
             // 
             this.timerMB.Tick += new System.EventHandler(this.TimerMB_Tick);
             // 
-            // panel1
+            // pictDownload
             // 
-            this.panel1.Controls.Add(this.LblUpload);
-            this.panel1.Controls.Add(this.LblDownload);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(31, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(80, 38);
-            this.panel1.TabIndex = 7;
+            this.pictDownload.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictDownload.Image = global::WinNetMeter.Shell.Properties.Resources.down_white_16px;
+            this.pictDownload.Location = new System.Drawing.Point(7, 18);
+            this.pictDownload.Name = "pictDownload";
+            this.pictDownload.Size = new System.Drawing.Size(12, 12);
+            this.pictDownload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictDownload.TabIndex = 2;
+            this.pictDownload.TabStop = false;
+            this.pictDownload.WaitOnLoad = true;
+            // 
+            // pictUpload
+            // 
+            this.pictUpload.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictUpload.Image = global::WinNetMeter.Shell.Properties.Resources.up_white_16px;
+            this.pictUpload.Location = new System.Drawing.Point(7, 5);
+            this.pictUpload.Name = "pictUpload";
+            this.pictUpload.Size = new System.Drawing.Size(12, 12);
+            this.pictUpload.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictUpload.TabIndex = 3;
+            this.pictUpload.TabStop = false;
+            this.pictUpload.WaitOnLoad = true;
+            // 
+            // LblDownload
+            // 
+            this.LblDownload.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.LblDownload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDownload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblDownload.Location = new System.Drawing.Point(31, 17);
+            this.LblDownload.Name = "LblDownload";
+            this.LblDownload.Size = new System.Drawing.Size(77, 12);
+            this.LblDownload.TabIndex = 6;
+            this.LblDownload.Text = "......";
+            this.LblDownload.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.LblDownload.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            // 
+            // LblUpload
+            // 
+            this.LblUpload.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.LblUpload.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUpload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblUpload.Location = new System.Drawing.Point(31, 3);
+            this.LblUpload.Name = "LblUpload";
+            this.LblUpload.Size = new System.Drawing.Size(77, 12);
+            this.LblUpload.TabIndex = 5;
+            this.LblUpload.Text = "......";
+            this.LblUpload.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.LblUpload.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             // 
             // UserControl1
             // 
@@ -172,26 +162,22 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Black;
             this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.LblUpload);
+            this.Controls.Add(this.LblDownload);
             this.Controls.Add(this.pictUpload);
             this.Controls.Add(this.pictDownload);
-            this.Controls.Add(this.panel1);
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(111, 38);
             this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictDownload)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictUpload)).EndInit();
-            this.contextMenuStrip1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictDownload;
-        private System.Windows.Forms.PictureBox pictUpload;
         private System.Windows.Forms.Timer timerAuto;
-        private MyLabel LblUpload;
-        private MyLabel LblDownload;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem configurationToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem monitoringToolStripMenuItem;
@@ -200,6 +186,9 @@
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.Timer timerKB;
         private System.Windows.Forms.Timer timerMB;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictDownload;
+        private System.Windows.Forms.PictureBox pictUpload;
+        private MyLabel LblDownload;
+        private MyLabel LblUpload;
     }
 }

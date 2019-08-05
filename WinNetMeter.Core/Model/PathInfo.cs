8b4fd38c770bde20fc1a34189b61d4388e016632
Muhammad Arfan <muchammadarfan76@gmail.com>
2019-08-05 21:19:19 +0700

    Added global environment variables for pathinfo

---
+++ WinNetMeter.Core/Model/PathInfo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinNetMeter.Core.Model
+{
+    public class PathInfo
+    {
+
+        // Directory
+        public static readonly string baseApp = AppDomain.CurrentDomain.BaseDirectory;
+        public static readonly string baseLocalData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public static readonly string companyFolderName = "WinTenDev";
+        public static string tempFolderName = "temp";
+        public static string updateFolderName = "update";
+
+        // File
+        public static readonly string toolBarInstallerFileName = "toolbarInstaller.bat";
+        public static string toolBarUninstallerFileName = "toolbarUninstaller.bat";
+
+        public static readonly string tempLocation = $@"{baseLocalData}\{companyFolderName}\{tempFolderName}";
+        public static readonly string updateLocation = $@"{baseLocalData}\{companyFolderName}\{updateFolderName}";
+        public static readonly string toolBarInstallerLocation = $@"{baseLocalData}\{companyFolderName}\{tempFolderName}\{toolBarInstallerFileName}";
+        public static readonly string toolBarUninstallerLocation = $@"{baseLocalData}\{companyFolderName}\{tempFolderName}\{toolBarUninstallerFileName}";
+        public static readonly string shellLocation = $"{baseApp} + WinNetMeter.Shell.dll";
+    }
+}

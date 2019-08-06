147921980032e2f18161863754e39a368fb831f6
Azhe Kun <azhe403@gmail.com>
2019-08-06 22:18:30 +0700

    wpf ui testing

---
+++ WinNetMeter.UI/Autoloader/ViewAutoloader.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using WinNetMeter.UI.Views;
+
+namespace WinNetMeter.UI.Autoloader
+{
+    internal class ViewAutoloader : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<GeneralPage>();
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RequestNavigate("MainRegion", "GeneralPage");
+        }
+    }
+}

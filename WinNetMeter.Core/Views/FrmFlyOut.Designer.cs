004bfde8db05bea8a8d509fcc9c9ed9a02215ac6
Muhammad Arfan <muchammadarfan76@gmail.com>
2019-09-08 09:21:41 +0700

    Add popup form

---
+++ WinNetMeter.Core/Views/FrmFlyOut.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace WinNetMeter.Core.Views
+{
+    partial class FrmFlyOut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmFlyOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(404, 443);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmFlyOut";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "FrmFlyOut";
+            this.Load += new System.EventHandler(this.FrmFlyOut_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WinNetMeter.Core/Views/FrmFlyOut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinNetMeter.Core.Views
+{
+    public partial class FrmFlyOut : Form
+    {
+        public FrmFlyOut()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFlyOut_Load(object sender, EventArgs e)
+        {
+            Screen rightmost = Screen.AllScreens[0];
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.Right > rightmost.WorkingArea.Right)
+                    rightmost = screen;
+            }
+
+            this.Left = rightmost.WorkingArea.Right - this.Width;
+            this.Top = rightmost.WorkingArea.Bottom - this.Height;
+        }
+
+
+    }
+}

be72c149e0ed22136b98caa5f21d593908becebf
Muhammad Arfan <muchammadarfan76@gmail.com>
2019-11-26 09:15:40 +0700

    Update Main.Designer.cs

--- WinNetMeter/Main.Designer.cs
+++ WinNetMeter/Main.Designer.cs
@@ -30,7 +30,6 @@ namespace WinNetMeter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.PanelMainMenu = new System.Windows.Forms.Panel();
             this.LineAboutSeparator = new System.Windows.Forms.GroupBox();
@@ -57,7 +56,7 @@ namespace WinNetMeter
             this.about = new WinNetMeter.UserControls.Pages.About();
             this.tabUpdateRecovery = new System.Windows.Forms.TabPage();
             this.updaterPage1 = new WinNetMeter.UserControls.Pages.UpdaterPage();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer();
             this.colorSelector = new System.Windows.Forms.ColorDialog();
             this.panel2 = new System.Windows.Forms.Panel();
             this.LblTitlePages = new System.Windows.Forms.Label();
@@ -238,10 +237,10 @@ namespace WinNetMeter
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 30);
+            this.label1.Size = new System.Drawing.Size(136, 30);
             this.label1.TabIndex = 1;
             this.label1.Text = "WinNetMeter";
             // 

728ff07dc389905a54284e01b01c1a37b93de017
Azhe-kun <azhe403@gmail.com>
2020-02-15 00:11:16 +0700

    add typeHelper

---
+++ WinNetMeter.Core/Helper/TypeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinNetMeter.Core.Helpers
+{
+    public static class TypeHelper
+    {
+        public static bool ToBool(this object obj)
+        {
+            return Convert.ToBoolean(obj);
+        }
+
+        public static int ToInt(this object obj)
+        {
+            return Convert.ToInt32(obj);
+        }
+    }
+}

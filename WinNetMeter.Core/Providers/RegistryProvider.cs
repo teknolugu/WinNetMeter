b236fb3d367eb817ea9cf3fece28603c9badc173
Azhe-kun <azhe403@gmail.com>
2020-02-15 00:11:55 +0700

    add registry provider

---
+++ WinNetMeter.Core/Providers/RegistryProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+
+namespace WinNetMeter.Core.Providers
+{
+    public static class RegistryProvider
+    {
+        private static readonly RegistryKey registryBase = Registry.CurrentUser.OpenSubKey(@"Software", true);
+        private static RegistryKey _registryRoot;
+        private static string rootPath = @"WinTenDev\NetMeter";
+
+        public static void Init()
+        {
+            registryBase.CreateSubKey(rootPath);
+            _registryRoot = registryBase.OpenSubKey(rootPath,true);
+
+            _registryRoot.CreateSubKey("General");
+            _registryRoot.CreateSubKey("Database");
+            _registryRoot.CreateSubKey("Style");
+        }
+
+        public static void Reset()
+        {
+            if (_registryRoot == null)
+            {
+                return;
+            }
+
+            foreach (string subKey in _registryRoot.GetSubKeyNames())
+            {
+                try
+                {
+                    _registryRoot.DeleteSubKeyTree(subKey);
+                }
+                catch
+                {
+                }
+            }
+
+            Init();
+        }
+
+        public static bool IsKeyExist(string regPath)
+        {
+            var regKey =_registryRoot.OpenSubKey(regPath);
+            return regKey != null;
+        }
+
+        public static void WriteToRegistry(string path, string keyReg, string valReg)
+        {
+            var exeKey = _registryRoot.OpenSubKey(@path, true);
+            exeKey?.SetValue(keyReg, valReg, RegistryValueKind.String);
+        }
+
+        public static object ReadFromRegistry(string path, string keyReg)
+        {
+            var regKey = _registryRoot.OpenSubKey(path, true);
+            return regKey?.GetValue(keyReg);
+        }
+    }
+}

b8f29114b8746a8aeb993b3db1717d8464f8fa1f
Azhe-kun <azhe403@gmail.com>
2020-02-15 00:13:57 +0700

    static class settings example

---
+++ WinNetMeter.Core/Model/Settings.cs
@@ -0,0 +1,20 @@
+﻿using WinNetMeter.Core.Helpers;
+using WinNetMeter.Core.Providers;
+
+namespace WinNetMeter.Core.Model
+{
+    public static class Settings
+    {
+        public static bool EnableMonitoring
+        {
+            get => RegistryProvider.ReadFromRegistry("General", "Monitoring").ToBool();
+            set => RegistryProvider.WriteToRegistry("General", "Monitoring", value.ToString());
+        }
+
+        public static bool EnableAutoUpdate
+        {
+            get => RegistryProvider.ReadFromRegistry("General", "AutoUpdate").ToBool();
+            set => RegistryProvider.WriteToRegistry("General", "AutoUpdate", value.ToString());
+        }
+    }
+}

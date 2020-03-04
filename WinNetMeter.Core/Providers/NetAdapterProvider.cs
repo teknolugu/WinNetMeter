c2154d12527ae15d31c57cc1e9942b72500764dc
Azhe Kun <azhe403@gmail.com>
2020-03-04 22:27:47 +0700

    add netAdapter provider

---
+++ WinNetMeter.Core/Providers/NetAdapterProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinNetMeter.Core.Providers
+{
+    class NetAdapterProvider
+    {
+        public List<string> GetNetworkInterface()
+        {
+            List<string> adapters = new List<string>();
+            PerformanceCounterCategory category = new PerformanceCounterCategory("Network Interface");
+            foreach (string name in category.GetInstanceNames())
+            {
+                adapters.Add(name);
+            }
+            return adapters;
+        }
+    }
+}

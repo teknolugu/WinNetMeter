4cbc2ffbd4bd30aa47fcd4265db97a882d4eee71
Azhe-kun <azhe403@gmail.com>
2020-03-07 13:11:09 +0700

    add evolve

---
+++ WinNetMeter.Core/Providers/EvolveProvider.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using System;
+using WinNetMeter.Core.Model;
+
+namespace WinNetMeter.Core.Providers
+{
+    public class EvolveProvider
+    {
+        public static void Initialize()
+        {
+            try
+            {
+                var cnx = SqliteProvider.InitSqLite();
+                var evolve = new Evolve.Evolve(cnx, Log.Information)
+                {
+                    Locations = new[] { Settings.AppDirectory + @"\Storage\Migration" },
+                    IsEraseDisabled = true,
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed.", ex);
+                throw;
+            }
+        }
+    }
+}
